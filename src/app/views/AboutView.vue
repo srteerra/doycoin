118d18086cb30bcffb756385bc18d0c7516d6c1b
Guapura89 <betocrack1228@gmail.com>
2022-08-16 21:26:18 -0600

    Merge completed

--- src/app/views/AboutView.vue
+++ src/app/views/AboutView.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
 	<div>
 		<b-container class="py-5">
@@ -30,28 +29,12 @@
 			</b-row>
 			<!-- Members section -->
 			<b-row class="pb-4 pt-5">
-				<h3 class="font-weight-bold text-center w-100">
-					<span>
-						<img
-							src="../assets/vectors/leave-5.png"
-							alt=""
-							style="width: 30px"
-							class="pb-2"
-						/>
-					</span>
+				<h2 class="font-weight-bold text-center w-100">
 					Members
-					<span>
-						<img
-							src="../assets/vectors/leave-5.png"
-							alt=""
-							style="width: 30px; transform: scaleX(-1);"
-							class="pb-2"
-						/>
-					</span>
-				</h3>
+				</h2>
 			</b-row>
 			<b-row class="py-5">
-				<b-col cols="12" sm="12" md="8" lg="4" class="memberCard">
+				<b-col cols="12" sm="12" md="8" lg="4" class="my-2">
 					<MemberCard
 						nickName="Carlos Sanchez"
 						position="CTO"
@@ -60,7 +43,7 @@
 						instaURL="https://www.instagram.com/"
 					/>
 				</b-col>
-				<b-col cols="12" sm="12" md="8" lg="4" class="memberCard">
+				<b-col cols="12" sm="12" md="8" lg="4" class="my-2">
 					<MemberCard
 						nickName="Angel Lopez"
 						position="CEO/Founder"
@@ -69,7 +52,7 @@
 						instaURL="https://www.instagram.com/"
 					/>
 				</b-col>
-				<b-col cols="12" sm="12" md="8" lg="4" class="memberCard">
+				<b-col cols="12" sm="12" md="8" lg="4" class="my-2">
 					<MemberCard
 						nickName="Jonathan Ocampo"
 						position="CSO"
@@ -86,6 +69,11 @@
 				<b-row class="my-5">
 					<b-col cols="12" md="12" lg="6" class="mb-5">
 						<h2 class="font-weight-bold">Get in touch</h2>
+						<img
+							class="contact-stack-img"
+							src="../assets/vectors/stack-1.png"
+							alt="stack"
+						/>
 						<p>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
 							nisi eaque! Assumenda quos cum perferendis provident aperiam earum
@@ -101,17 +89,118 @@
 				</b-row>
 			</b-container>
 		</section>
-		<!-- our blog section -->
-		<b-row class="py-5">
-			<b-col class="pt-5">
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-					doloremque culpa id magni quos itaque quasi similique voluptas atque.
-					Ea odit excepturi repudiandae error itaque. Asperiores tempora eius
-					qui sequi!
-				</p>
-			</b-col>
-		</b-row>
+		<!-- Roadmap section -->
+		<b-container class="roadmap">
+			<b-row class="pt-5">
+				<b-col cols="12" md="6" class="stage-content my-3">
+					<img src="../assets/vectors/tree-5.png" alt="tree-5" />
+					<ul class="mx-3">
+						<h3 class="text-dark font-weight-bold mx-auto pt-5 pt-sm-0">
+							Stage 1
+						</h3>
+						<li>Pancakeswap Launch</li>
+						<li>Launch of Socials</li>
+						<li>Launch of Dapp v1.0</li>
+						<li>Organic Growth</li>
+						<li>Swing Trading</li>
+					</ul>
+				</b-col>
+				<b-col cols="12" md="6"></b-col>
+			</b-row>
+			<b-row class="pt-5">
+				<b-col cols="12" md="6"></b-col>
+				<b-col cols="12" md="6" class="stage-content right-ul my-3">
+					<ul class="mx-3">
+						<h3 class="text-dark font-weight-bold mx-auto pt-5 pt-sm-0">
+							Stage 2
+						</h3>
+						<li>Twitter marketing</li>
+						<li>+500 holders</li>
+						<li>Disord server</li>
+						<li>Sponsorship Crypto Events</li>
+						<li>Interactive campaign</li>
+					</ul>
+					<img src="../assets/vectors/tree-10.png" alt="tree-10" />
+				</b-col>
+			</b-row>
+			<b-row class="pt-5">
+				<b-col cols="12" md="6" class="stage-content my-3">
+					<img src="../assets/vectors/tree-3.png" alt="tree-3" />
+					<ul class="mx-3">
+						<h3 class="text-dark font-weight-bold mx-auto pt-5 pt-sm-0">
+							Stage 3
+						</h3>
+						<li>Mobile App</li>
+						<li>+1000 holders</li>
+						<li>Listing on exchanges</li>
+						<li>NFT Platform</li>
+						<li>Official merch</li>
+					</ul>
+				</b-col>
+				<b-col cols="12" md="6"> </b-col>
+			</b-row>
+			<b-row class="pt-5">
+				<b-col cols="12" md="6"> </b-col>
+				<b-col cols="12" md="6" class="stage-content right-ul my-3">
+					<ul class="mx-3">
+						<h3 class="text-dark font-weight-bold mx-auto pt-5 pt-sm-0">
+							Stage 4
+						</h3>
+						<li>Rebranding</li>
+						<li>+5000 holders</li>
+						<li>Launch of Dapp v2.0</li>
+						<li>Massive marketing</li>
+						<li>Diversity Funding</li>
+					</ul>
+					<img src="../assets/vectors/tree-1.png" alt="tree-1" />
+				</b-col>
+			</b-row>
+		</b-container>
+
+		<!-- Give us a coffe secton -->
+
+		<b-container class="coffee mx-auto pb-lg-5">
+			<b-row class="mb-2">
+				<b-col class="text-center">
+					<h3 class="text-dark font-weight-bold mx-auto pt-5 pt-sm-0">
+						<span>
+							<img
+								src="../assets/vectors/leave-5.png"
+								alt=""
+								style="width: 30px"
+								class="pb-2"
+							/>
+						</span>
+						Give us a coffee
+						<span>
+							<img
+								src="../assets/vectors/leave-5.png"
+								alt=""
+								style="width: 30px; transform: scaleX(-1);"
+								class="pb-2"
+							/>
+						</span>
+					</h3>
+					<p class="font-weight-light">
+						If you'd like to donate to the Devs, send ETH, SHIB, USDT here.
+						Thank you for your support!
+					</p>
+				</b-col>
+			</b-row>
+			<b-row class="mb-5">
+				<b-col>
+					<b-button
+						id="devAdd"
+						size="md"
+						class="py-4 px-3 my-3 w-100 d-inline-block text-truncate font-weight-bold"
+						@click="copyAddress('0xb37ecc72b98d7004c284fda84315eac16903bda3')"
+					>
+						0xc351155C80aCD043BD5F8FE7ffc8536af1fF9375
+						<span class="pl-1"><b-icon-clipboard /></span>
+					</b-button>
+				</b-col>
+			</b-row>
+		</b-container>
 	</div>
 </template>
 
@@ -131,8 +220,13 @@ export default {
 	box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 }
 
-.memberCard {
-	margin: 5px auto;
+.title-vector {
+	width: 70px;
+	padding-bottom: 40px;
+}
+
+#mirror {
+	transform: scaleX(-1);
 }
 
 .trees-section {
@@ -151,6 +245,12 @@ export default {
 .contact {
 	background-color: #262626;
 	color: white;
+	margin-top: 200px;
+	.contact-stack-img {
+		width: 200px;
+		position: absolute;
+		top: -263px;
+	}
 }
 
 .address {
@@ -159,8 +259,22 @@ export default {
 
 .roadmap {
 	img {
-		width: 100px;
+		width: 200px;
+		height: 200px;
+	}
+	.stage-content {
+		display: flex;
+		align-items: center;
+		ul {
+			display: inline-block;
+		}
+	}
+	.right-ul {
+		text-align: right;
 	}
 }
+
+.coffee {
+	margin: 200px 0;
+}
 </style>
->>>>>>> d2de69989bb86546f9ccb0da9294daf965f78620
